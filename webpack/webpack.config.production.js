13e391ed8f70795f4c0c448e430271e4096fe9a5
suches <908202921@qq.com>
2018-08-02 16:28:52 +0800

    up

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -11,11 +11,13 @@ config.plugins = [
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify('production')
   }),
+  /*
   new webpack.optimize.UglifyJsPlugin({
     compressor: {
       warnings: false
     }
   })
+  */
 ];
 
 module.exports = config;
